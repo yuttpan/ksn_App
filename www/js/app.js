1e8b3fac7da71ebf763ed3725c36f5a05ccad505
yuttpan <yuttpanin@gmail.com>
2016-07-17 19:58:46 +0700

    ทำตาราง

--- www/js/app.js
+++ www/js/app.js
@@ -83,3 +83,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
 });
+
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -56,3 +56,29 @@ angular.module('starter.controllers', [])
   console.log($stateParams);
 })
 
+.controller('MyCtrl', function($scope) {
+  
+  var ctrl = this;
+  
+  ctrl.add = add;
+  ctrl.data = [
+    {
+      name: "AiA",
+      code: "AI101",
+      limit: 25000,
+      account: "Life Insurance"
+    },
+    {
+      name: "Cargills",
+      code: "CF001",
+      limit: 30000,
+      account: "Food City"
+    }
+  ]
+  
+  ////////
+  function add(index) {
+    window.alert("Added: " + index);
+  }
+  
+});
